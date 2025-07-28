15845f4a87a10879eec582fb8f7729c3f393bb06
Mamun-Ali <mamunalobaid@gmail.com>
2025-07-28 17:24:43 +0300

    feat(init.lua): passed prettier as formater for a number languages

--- init.lua
+++ init.lua
@@ -777,6 +777,16 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        astro = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        css = { 'prettier' },
+        html = { 'prettier' },
+        json = { 'prettier' },
+        scss = { 'prettier' },
+        sass = { 'prettier' },
       },
     },
   },
